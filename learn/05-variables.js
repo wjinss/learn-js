7f351f3347598906732c867bd3ef40ad0f7cc171
wjinss <aastu4343@gmail.com>
2025-06-10 14:41:48 +0900

    [실습] 변수 및 상수 이름
    - 변하지 않는 것
    - 변하는 것

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -33,3 +33,46 @@ console.log("현재 내가 가진 사과의 개수 = ", total_apple);
 // 알파벳 a-z, A-Z 숫자 0~9, _, $ 등의 기호로 구성되어야 됨
 // 변수 이름은 숫자로 시작하면 안됨
 // 변수 이름에 예약어는 사용할 수 없음
+
+// 변수 선언(할당 없이)
+let addBuyApples; // 기본값은 undefined
+
+// 선언된 변수에 값 할당
+addBuyApples = 27;
+
+// let 변수 vs var 변수
+var myName;
+console.log(1, myName);
+
+myName = "wjinss";
+console.log(2, myName);
+
+myName = "우리둥절";
+console.log(3, myName);
+
+// 블록 스코프 예시
+{
+  var myName;
+  console.log(1, myName);
+
+  myName = "yamoo9";
+  console.log(2, myName);
+}
+
+myName = "y9";
+console.log(3, myName);
+
+{
+  let myName2;
+  console.log(1, myName2);
+
+  myName2 = "yamoo9";
+  console.log(2, myName2);
+}
+
+// myName2 = "y9";
+// console.log(3, myName2);
+
+// let 변수 vs const 변수(상수, constant)
+// - 변수 : 시간의 흐름에 따라 값이 변경됨
+// - 상수 : 처음 선언 시, 할당된 값이 이후 변경되지 않음
---
+++ learn/06-practice.js
@@ -0,0 +1,39 @@
+// 1. `const` 키워드를 사용해 값이 변하지 않는 상수를 선언해봅니다.
+//     1. "하루는 24시간"
+const dayToHours = 24;
+console.log("하루는 ", dayToHours, "시간");
+//     2. "물의 어는 점은 섭씨 0도"
+const freezingPoint = 0;
+console.log("물의 어는 점은 섭씨 ", freezingPoint, "도");
+//     3. "삼각형 내각의 합은 180도"
+const triangleAllCabinet = 180;
+console.log("삼각형 내각의 합은 ", triangleAllCabinet, "도");
+//     4. "지구의 중력 가속도는 9.8 m/s²"
+const earthGForc = 9.8;
+console.log("지구의 중력 가속도는 ", earthGForc, "  m/s²");
+// 2. 다음 내용을 코드로 작성해 결과 값을 콘솔 패널에 출력해보세요.
+//     1. `let` 키워드를 사용해 `sum` 변수를 선언하고 값을 `0`으로 설정합니다.
+let sum = 0;
+console.log("sum = ", sum);
+
+//     2. `num1`, `num2` 변수를 선언하고, 각각 `300`, `50`으로 값을 설정합니다.
+let num1 = 300;
+let num2 = 50;
+console.log("num1 = ", num1);
+console.log("num2 = ", num2);
+//     3. `num1`, `num2`의 합을 `sum` 변수에 할당합니다.
+sum = num1 + num2;
+console.log("num1+num2 = ", sum);
+
+// 3. 사람 이름, 나이, 오늘의 기온, 장바구니 상품 갯수, 집 주소, 현재 시간,
+// 로그인 상태, 피자 가격, 학생 수 등에 걸맞는 변수 이름을 정하고 적절한 값을 설정합니다.
+
+let peopleName = "우진";
+let age = 29;
+let todayTemperature = 26;
+let cartItemCount = 1;
+let homeAddress = "서울시 강서구 화곡동..";
+let currentTime = "14시 26분";
+let loginStatus = true;
+let pizzePrice = 24900;
+let studentCount = 43;
