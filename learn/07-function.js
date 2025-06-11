67f4dfccd0f7757ddd35846ee7bd9fa64a5aa66e
wjinss <aastu4343@gmail.com>
2025-06-11 10:14:43 +0900

    [학습] 함수에 관해
    - 변수, 매개변수 기본값(undefined)
    - 함수의 암묵적 반환값(undefined)
    - 함수에서 명시적 반환값을 할때는 return 키워드를 사용한다.
    - console.log()는 개발자 도구의 콘솔 패널에 출력만 할 뿐, 개발과 관련없다

--- learn/07-function.js
+++ learn/07-function.js
@@ -41,11 +41,19 @@ function 함수이름() {
 // 4. 예약어는 사용할 수 없다
 
 // ---------------------------
-// 매겨변수(Parameters)
+// 매개변수(Parameters)
+// ---------------------------
+
+// 전역변수 (global scope variables)
+// 코드 실행 범위 어디서나 접근 가능한 변수
+// let yeat = 2026
 
 // 함수 선언
 function writeDiary(year, month, date, location) {
   // 함수 내부에 선언된 변수 > 함수 안에서 사용
+
+  // 지역변수 (local scope variables)
+  let mood = "행복감";
   console.log(
     year +
       "년 " +
@@ -59,3 +67,60 @@ function writeDiary(year, month, date, location) {
 }
 writeDiary(2025, 6, 15, "국립 중앙 박물관");
 writeDiary(2025, 8, 19, "을지로");
+
+// 변수 or 매개변수에 아무런 값이 할당되지 않으면 기본값으로 undefined가 할당된다
+let day;
+writeDiary();
+
+// ---------------------------
+// 반환문(return)
+// ---------------------------
+function add(num1, num2) {
+  // 브라우저 콘솔에 num1 + num2가 계산된 값이 기록된다
+  console.log("콘솔에 출력된 계산 결과 = ", num1 + num2);
+
+  // 함수에서 처리(계산)된 값을 반환하려면 return 키워드가 필요하다
+  return num1 + num2;
+}
+
+// 함수가 실행되어 값을 반환한다면 그 값을 변수에 기억하려한다.
+let result = add(10, 9);
+console.log("결과값 = ", result);
+
+// 함수가 암묵적으로 반환하는 값
+// return으로 값을 반환하지 않으면 undefined가 암묵적으로 반환됨
+
+// ---------------------------
+// console.log() vs. 함수의 return
+// ---------------------------
+function 곱하기(숫자1, 숫자2) {
+  let 결과값 = 숫자1 * 숫자2; // 계산된 값
+  // console.log(결과값)
+
+  // 명시적 결과 반환
+  // return 결과값;
+
+  // 암묵적 결과 반환
+  return undefined;
+}
+
+function 화면에결과그리기() {
+  const 화면에그릴숫자값 = 곱하기(10, 20); // undefined
+  document.write(화면에그릴숫자값); // undefined > "undefined"
+}
+
+화면에결과그리기();
+
+// 전역변수를 선언만하고 함수 내부에서 정의할 경우에는 return을 사용할 필요 없이 전역변수의 값이 변할 수 있는데 이때에도 return이 없으면 함수는 undefined를 반환하나요?
+
+let y = "전역변수";
+
+function fn() {
+  // 전역 변수 변경
+  y = 0;
+
+  // 전역 변수를 변경만 하고 아무런 값을 명시적으로 반환하지 않는다.
+  // return undefined
+}
+
+console.log(fn()); // undefined
