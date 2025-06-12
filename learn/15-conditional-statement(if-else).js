069332daed6bd4053b5ce64f4a4f48f0dc71383f
wjinss <aastu4343@gmail.com>
2025-06-12 15:28:25 +0900

    학습] 조건문 : if ... else - if 블록 - else 블록 - else if 블록 - 비교 연산자 - 느슨한 비교 vs. 엄격한 비교

--- learn/15-conditional-statement(if-else).js
+++ learn/15-conditional-statement(if-else).js
@@ -45,3 +45,60 @@ console.log(Boolean(()=>{}));
 console.log(Boolean({}));
 console.log(Boolean([]));
 
+if (-100) {
+  console.log('this is truethy')
+} else {
+
+}
+
+x = 10
+y = 5
+
+// JavaScript는 상황에 따라 타입을 자동 변환
+
+if(x == y)  { console.log('this is truthy') }
+if(x === y) { console.log('this is truthy') }
+if(x != y)  { console.log('this is truthy') }
+if(x !== y) { console.log('this is truthy') }
+if(x < y) { console.log('this is truthy') }
+if(x <= y) { console.log('this is truthy') }
+if(x > y) { console.log('this is truthy') }
+if(x >= y) { console.log('this is truthy') }
+
+
+// 개발자 작성
+console.log(Boolean(24 == '24'))
+// JavaScript 엔진 처리 (타입 자동 변환, 느슨한 비교)
+console.log(Boolean('24' == '24'))
+
+// 개발자 작성
+console.log(Boolean(24 === '24'))
+// JavaScript 엔진 처리 (타입 변경 안함, 엄격한 비교)
+console.log(Boolean(24 === '24'))
+
+
+
+let condition = true
+
+// 조건 (참이다, 아니다 : 흑백논리)
+if (condition) {}
+else {}
+
+let 신호등불빛색 = '보라색'
+
+// 조건 1 (신호등 불이 빨간색)
+if (신호등불빛색 === '빨간색') {
+  console.log('건너면 안되요!')
+} 
+// 조건 2 (신호등 불이 노란색)
+else if (신호등불빛색 === '노란색') {
+  console.log('안전을 생각하면 그 자리에 멈추는 게 좋아요.')
+}
+// 조건 3 (신호등 불이 초록색)
+else if (신호등불빛색 === '초록색') {
+  console.log('좌우 살피고 건너세요!')
+}
+// 이도 저도 아니면...
+else {
+  console.log(신호등불빛색 + '은 신호등 불빛색 중에 없어요.')
+}
