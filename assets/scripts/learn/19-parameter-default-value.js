03b7ff2eb66a7d4ba75e74542d7756e440c55680
wjinss <aastu4343@gmail.com>
2025-06-13 10:20:02 +0900

    [학습] 매개변수 기본값 설정, if문 생략 가능한 조건
    - 인수가 없으면, 매개변수 값은 undefined
    - 함수 안에서 조건문 처리
    - 명시적 반환값 설정
    - if문의 블록{}은 내부 코드가 한줄이면 생략 가능
    - 함수 내부에서 return키워드를 확인하면 함수는 값을 반환하고 함수를 종료

--- assets/scripts/learn/19-parameter-default-value.js
+++ assets/scripts/learn/19-parameter-default-value.js
@@ -2,16 +2,31 @@
 // 📌 함수 매개변수 기본값 설정 (조건문 활용)
 // --------------------------------------------------------------------------
 
-function getMoney(price, unit) {
+function getMoney(price /*  필수값 */, unit/*  옵션 */) {
   // unit 매개변수(옵션)
   // 설정 가능한 값: '달러' 또는 '원화'
   // 기본 값: '원'
 
+  // 만약 사용자가 전달한 인수에 의해 unit 매개변수 값이 undefined로 설정된다면
+    // if(unit === undefined){
+    if(!unit){
+      // '원'값을 unit 매개변수의 기본값으로 설정한다
+      unit = "원"
+    }
+// if문의 {}내 코드가 한줄이면 {}생략 가능
 
+    // 사용자가 전달한 인수 값이 '달러'인 경우,
+    // 문자 연결하는 방법을 달리해서 결과값을 반환한다.
+    else if(unit === '$') return '$' + price
+
+    // 명시적으로 반환값 설정
+    return price + unit
 }
 
-let moneyKR = getMoney(5000 + 2000 + 1500 + 500) // '10000원'
+let moneyKR = getMoney(5000 + 2000 + 1500 + 500) // '9000원'
 let moneyUS = getMoney(10.5 + 90 - 20 + 0.25, '$') // '$80.75'
 
 console.log(moneyKR)
 console.log(moneyUS)
+
+
