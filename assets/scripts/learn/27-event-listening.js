88051d35c01f0df7c48c806dfd96985827cb5ed1
wjinss <aastu4343@gmail.com>
2025-06-16 14:23:04 +0900

    [학습] 이벤트 리스너에서 함수를 실행시키지 않고 호출하는 이유

--- assets/scripts/learn/27-event-listening.js
+++ assets/scripts/learn/27-event-listening.js
@@ -47,3 +47,52 @@ clickableBtn.addEventListener("click", () => {
 //     console.log('clicked clickable button')
 //   }
 // )
+
+// 실습 ----------------------------
+
+function handelToggle() {
+  console.log("toggle triggered");
+}
+
+function inlineClick() {
+  console.log(`요소가 클릭됨!!!!`);
+  addEvent.classList.toggle("is-clicked");
+}
+
+const addEvent = document.querySelector(".addEvent");
+addEvent.addEventListener("click", () => {
+  console.log(`요소가 클릭됨!!!!`);
+  addEvent.classList.toggle("is-clicked");
+});
+
+// addEventListener('click',alertLog)에서 alertLog에 ()를 안붙이는 이유
+const button = document.querySelector("button");
+
+function alertLog(e) {
+  console.log("clicked button");
+  button.classList.toggle("clicked");
+
+  // 명시적 반환일 경우
+  // return { name: '나는 반환값이다.' }
+
+  // 함수 내부에서 함수를 반환하는 경우
+  return () => {
+    console.log("나는 함수가 반환한 함수이다!");
+  };
+
+  // 암묵적으로 반환
+  // return undefined
+}
+
+// alertLog
+const result = alertLog();
+console.log(result); // undefined
+
+// 함수 이름만 참조
+// button.addEventListener('click', alertLog)
+
+// 함수 이름으로 호출(실행)
+// 이벤트 연결 (binding events)
+button.addEventListener("click", alertLog());
+
+// 즉, alertLog()를 호출하면 함수의 실행결과를 호출하는데 return이 없어 undefined를 반환한다
