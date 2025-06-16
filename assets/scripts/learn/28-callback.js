7c701d2be3e864c8073846cdd50afd00824426a0
wjinss <aastu4343@gmail.com>
2025-06-16 15:01:57 +0900

    [학습] 레거시 이벤트 모델 vs. 모던 이벤트 모델 - 기본적으로 이벤트를 여럿 연결 할 수 없음 - 변수에는 단 하나의 값만 기억되기 때문

---
+++ assets/scripts/learn/28-callback.js
@@ -0,0 +1,47 @@
+// 문서에 있는 버튼 요소에 접근(찾기)
+// 전역 변수에 참조(왜? 객체 타입이니깐)
+const btn = document.querySelector(".clickable");
+
+// 버튼 요소에 이벤트 리스터(함수) 추가
+// 콜백 = 다른 함수에 인수로 전달될 함수  (나중에 실행되도록)
+// 이벤트 = 언제 발생할 지 알 수 없음 (비동기)
+
+// btn.addEventListener("click", () => {
+//   console.log(`해당 함수는 나중에 실행되므로 콜백!`);
+// });
+
+// // addEventListener()를 사용해 리스너를 추가하는 것은 n회 가능
+// btn.addEventListener("click", () => {
+//   console.log(`두번째 연결된 콜백!`);
+// });
+
+// 콜백 === 이중중첩함수? > false
+// 함수 이중 중첩 예시
+// function fn1() {
+//   function fn2() {}
+// }
+
+// 콜백 예시
+
+fn1(fn2);
+function fn1(callback) {
+  // 실행순서 1
+  console.log("fn1");
+  callback();
+}
+
+function fn2() {
+  // 실행순서 2
+  console.log("fn2, 콜백");
+}
+
+// 레거시 이벤트(onclick) vs. 모던 이벤트(addEventListener)
+// 변수에는 여라 값을 저장할 수 있다? 아니면 단 하나의 값만 저장할 수 있다?
+// 레거시 이벤트는 객체의 변수가 가진 속성이다.
+
+btn.onclick = function () {
+  document.body.classList.toggle("is-active");
+};
+btn.onclick = function () {
+  btn.classList.toggle("is-active");
+};
