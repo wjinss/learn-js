e94bc71b23175e9e3294c49d3d2738c0c4ebb5b5
wjinss <aastu4343@gmail.com>
2025-06-17 16:36:16 +0900

    [학습] 디버깅 - 문제 찾기 - 오류 메시지 검토 - 구문 오류 - 타입 오류 - 잘못된 선택자 - 문제 해결

---
+++ assets/scripts/debugs/01-debugs.js
@@ -0,0 +1,10 @@
+// 수정 전 ----------------------
+// const button = document.querySelector('button')
+
+// button.addEventListener('click' e => {
+//   console.log('버튼 클릭!'))
+// })
+// 수정 후 ----------------------
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => console.log("버튼 클릭!"));
