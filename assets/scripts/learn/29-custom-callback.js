a992305a54199f256c49ca3c1d64933e385ff78a
wjinss <aastu4343@gmail.com>
2025-06-17 09:49:05 +0900

    [학습] 이벤트 헬퍼(유틸) 함수 - helper functions - util functions

--- assets/scripts/learn/29-custom-callback.js
+++ assets/scripts/learn/29-custom-callback.js
@@ -9,16 +9,33 @@ callToAction(() => {
 //--------------------------------------------
 const button1 = document.querySelector(".button-1");
 const button2 = document.querySelector(".button-2");
+const body = document.querySelector("body");
 
 click(document.querySelector(".button-1"), () => console.log("button 1"));
 click(document.querySelector(".button-2"), () => console.log("button 2"));
 
-const handleClick = () => console.log("clicked button");
+// mouseLeave(button1, () => console.log(`mouseleave`));
+// mouseEnter(button2, () => console.log(`mouseenter`));
+
+// const handleClick = () => console.log("clicked button");
 // button.addEventListener('click', handleClick)
 
 // click(button1, handleClick)
 // click(button2, handleClick)
 
-function click(element, listener) {
-  element.addEventListener("click", listener);
-}
+const handleActive = () => {
+  body.classList.add("is-active");
+  button1.classList.add("is-active");
+  button2.classList.add("is-active");
+};
+
+const handleInactive = () => {
+  body.classList.remove("is-active");
+  button1.classList.remove("is-active");
+  button2.classList.remove("is-active");
+};
+
+button1.addEventListener("mouseenter", handleActive);
+button1.addEventListener("mouseleave", handleInactive);
+button2.addEventListener("mouseenter", handleActive);
+button2.addEventListener("mouseleave", handleInactive);
---
+++ assets/scripts/utils/events.js
@@ -0,0 +1,10 @@
+function click(element, listener) {
+  element.addEventListener("click", listener);
+}
+
+function mouseLeave(elm, listener) {
+  elm.addEventListener("mouseleave", listener);
+}
+function mouseEnter(elm, listener) {
+  elm.addEventListener("mouseenter", listener);
+}
