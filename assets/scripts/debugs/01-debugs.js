c0c495fd41d221e8ca22d7d556dac319e82bfead
wjinss <aastu4343@gmail.com>
2025-06-18 10:36:49 +0900

    [학습] 린터(검사도구) - ESLint 검사도구 설치 - 빠른 시작 구성 - 명령어 검사 - VS Code 확장 설치 및 활용

--- assets/scripts/debugs/01-debugs.js
+++ assets/scripts/debugs/01-debugs.js
@@ -7,4 +7,21 @@
 // 수정 후 ----------------------
 const button = document.querySelector(".button");
 
-button.addEventListener("click", (e) => console.log("버튼 클릭!"));
+button.addEventListener("click", () => console.log("버튼 클릭!"));
+
+let condition = true;
+
+if (condition) {
+  console.log("참입니다!");
+} else {
+  console.log("거짓입니다!");
+}
+
+let link;
+
+link = document.querySelector(".link");
+console.log(link);
+
+button.addEventListener("click", () => {
+  console.log("버튼 클릭!");
+});
