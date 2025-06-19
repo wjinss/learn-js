023942e6a58c56c9fcccc3b44e26c3a171ddf9f4
wjinss <aastu4343@gmail.com>
2025-06-19 13:10:23 +0900

    [학습] for문의 실행 흐름 1. 변수 선언 및 초깃값 할당 2. 조건 비교 후, 참인 경우 반복문 블록 코드 실행 3. 변수 값 변경 4. 2 -> 3 반복 후, 조건이 거짓이 되면 반복문 종료

--- assets/scripts/learn/33-loop.js
+++ assets/scripts/learn/33-loop.js
@@ -61,65 +61,86 @@ if (wantToDraw) {
 // ----------------------------
 // 흐름 제어(continue, break)
 
-(() => {
-  let drawCount = 20;
-  let i = 0;
+// (() => {
+//   let drawCount = 20;
+//   let i = 0;
 
-  const draw = (n) => {
-    console.log("도화지에 그림을 그린 횟수 = " + i);
-  };
-  // continue > 건너뛰기
-  // while (i++ < drawCount /* 0 ~ 19 */) {
-  //   if (i > 10 && i <= 15) {
-  //     continue;
-  //   }
-  //   draw(i);
-  // }
+//   const draw = (n) => {
+//     console.log("도화지에 그림을 그린 횟수 = " + i);
+//   };
+//   // continue > 건너뛰기
+//   // while (i++ < drawCount /* 0 ~ 19 */) {
+//   //   if (i > 10 && i <= 15) {
+//   //     continue;
+//   //   }
+//   //   draw(i);
+//   // }
 
-  //break > 종료시키기
-  while (i++ < drawCount /* 0 ~ 19 */) {
-    if (i === 5) {
-      break;
-    }
-    draw(i);
-  }
+//   //break > 종료시키기
+//   while (i++ < drawCount /* 0 ~ 19 */) {
+//     if (i === 5) {
+//       break;
+//     }
+//     draw(i);
+//   }
 
-  console.log("last i =", i);
-})();
+//   console.log("last i =", i);
+// })();
 
 // ----------------------------
 // 반복문(do ...while)
 // while vs. do ...while
-(() => {
-  let condition = false;
+// (() => {
+//   let condition = false;
 
-  //while문
-  //조건이 거짓이면 1번도 실행 안함
-  while (condition) {
-    console.log(`condition is true`);
-  }
+//   //while문
+//   //조건이 거짓이면 1번도 실행 안함
+//   while (condition) {
+//     console.log(`condition is true`);
+//   }
 
-  //do ...while문
-  //조건이 거짓이어도 1번은 반드시 실행
-  do {
-    console.log(`condition is true`);
-  } while (condition);
-})();
+//   //do ...while문
+//   //조건이 거짓이어도 1번은 반드시 실행
+//   do {
+//     console.log(`condition is true`);
+//   } while (condition);
+// })();
 
 // ----------------------------
 // 사용자 입력 검증(do ...while)
 // "1부터 10까지 숫자를 입력하세요"
 // do...while 문 사용 예시
 
-{
-  let userInput;
+// {
+//   let userInput;
 
-  do {
-    userInput = parseInt(prompt("1부터 10까지 숫자를 입력하세요"), 10);
-    if (Number.isNaN(userInput) || userInput < 1 || userInput > 10) {
-      console.error("입력 값이 1부터 10까지 숫자여야 합니다.");
-    }
-  } while (Number.isNaN(userInput) || userInput < 1 || userInput > 10);
+//   do {
+//     userInput = parseInt(prompt("1부터 10까지 숫자를 입력하세요"), 10);
+//     if (Number.isNaN(userInput) || userInput < 1 || userInput > 10) {
+//       console.error("입력 값이 1부터 10까지 숫자여야 합니다.");
+//     }
+//   } while (Number.isNaN(userInput) || userInput < 1 || userInput > 10);
 
-  console.log("사용자가 입력한 값은 " + userInput + "입니다.");
-}
+//   console.log("사용자가 입력한 값은 " + userInput + "입니다.");
+// }
+
+// ----------------------------
+// 반복문 for
+
+// while문 vs. for문
+
+// while문
+(() => {
+  let i = 0;
+  while (i < 10) {
+    console.log(`while문`);
+    ++i;
+  }
+})();
+
+// for문
+(() => {
+  for (let i = 0; i < 10; ++i) {
+    console.log(`for문`);
+  }
+})();
