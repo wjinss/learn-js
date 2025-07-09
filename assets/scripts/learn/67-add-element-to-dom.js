14bcd1c616a589ae94ecef5174dceeb18a15c871
wjinss <aastu4343@gmail.com>
2025-07-09 10:44:09 +0900

    [학습] DOM에 생성된 요소 추가 - parentElement.appendChild(childElement)

---
+++ assets/scripts/learn/67-add-element-to-dom.js
@@ -0,0 +1,43 @@
+// --------------------------------------------
+// DOM에 요소 추가
+// --------------------------------------------
+
+// 요소 동적 생성(create element)
+
+(() => {
+  // 동적 생성된 요소들을 마운트할 요소
+  const mountElement = document.querySelector(".mount");
+
+  // <section>
+  const sectionElement = document.createElement("section");
+  console.log(sectionElement);
+
+  // 생성된 요소에 클래스 이름 설정(추가)
+  sectionElement.classList.add("sectioning-content");
+
+  // 커스텀 속성 설정(추가)
+  sectionElement.dataset.category = "sports";
+
+  // 텍스트 콘텐츠 추가
+  sectionElement.textContent = `스포츠는 역시 멋사!`;
+
+  // HTML 콘텐츠 추가(덮어쓰기)
+  sectionElement.innerHTML = `<h2>${sectionElement.textContent}</h2>`;
+
+  // <table> 요소 생성
+  const tableElement = document.createElement("table");
+  // 생성된 요소에 클래스 이름 설정(추가)
+  tableElement.classList.add("teble-content");
+  // 커스텀 속성 설정(추가)
+  tableElement.dataset.type = "text";
+  // 표 제목 텍스트 콘텐츠 추가
+  tableElement.textContent = `자고싶다`;
+  // 표 제목 HTML 콘텐츠 추가(덮어쓰기)
+  tableElement.innerHTML = `<caption>${tableElement.textContent}</caption>`;
+
+  console.log(tableElement);
+
+  // 요소를 부모 요소의 마지막 자식으로 추가(append child)
+  mountElement.appendChild(sectionElement);
+  mountElement.appendChild(tableElement);
+})();
